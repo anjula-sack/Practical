2160b242996078bfefd4d9e5585d6a3eb379a2db
anjula-sack <anjulashanaka@gmail.com>
2020-07-22 20:21:12 +0530

    Fix getting total bug

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,6 @@ class Home extends React.Component {
     items = [];
     size = 'all';
     type = 'all';
-    updatedItems = [];
 
     constructor(props) {
         super(props);
@@ -19,8 +18,6 @@ class Home extends React.Component {
 
     componentDidMount() {
         this.fetchItems();
-        this.updatedItems = JSON.parse(localStorage.getItem('items'));
-        console.log(this.updatedItems);
     }
 
     fetchItems = () => {
@@ -59,13 +56,8 @@ class Home extends React.Component {
     }
 
     render() {
-        let total = 0;
-        this.updatedItems.forEach( item => {
-            total += item.qty * item.details.price;
-        })
         return (
             <Container style={{ marginTop: 20 }}>
-                <h1>Total = {total}</h1>
                 <Row>
                 <Col>
                     <span>Size</span>
